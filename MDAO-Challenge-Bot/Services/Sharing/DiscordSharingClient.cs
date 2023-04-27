419a4982f13b54a0340d33d9ce2656a3d5d0f100
Playwo <eliaswolf2001@t-online.de>
2023-04-27 23:08:44 +0200

    Add docker setup

--- MDAO-Challenge-Bot/Services/Sharing/DiscordSharingClient.cs
+++ MDAO-Challenge-Bot/Services/Sharing/DiscordSharingClient.cs
@@ -11,38 +11,42 @@ public class DiscordSharingClient : Singleton
     [Inject]
     private readonly DiscordWebhookClient WebhookClient = null!;
 
-    private static string MakeAirtableDiscordMessage(AirtableChallenge challenge)
+    private static Embed MakeAirtableChallengeEmbed(AirtableChallenge challenge)
     {
-        return $"""
-    {challenge.Name} | Deadline to submit: <t:{challenge.EndDate.ToUnixTimeSeconds()}:R> 
-
-    https://metricsdao.notion.site/Bounty-Programs-d4bac7f1908f412f8bf4ed349198e5fe
-    """;
+        return new EmbedBuilder()
+            .WithColor(Color.LightOrange)
+            .WithTitle(challenge.Name)
+            .AddField("Submission Deadline", $"<t:{challenge.EndDate.ToUnixTimeSeconds()}:R>")
+            .AddField("Claim now", "https://metricsdao.notion.site/Bounty-Programs-d4bac7f1908f412f8bf4ed349198e5fe")
+            .Build();
     }
 
     public async Task ShareAsync(AirtableChallenge challenge)
     {
-        //await WebhookClient.SendMessageAsync(MakeAirtableDiscordMessage(challenge));
+        await WebhookClient.SendMessageAsync(
+            embeds: new[] { MakeAirtableChallengeEmbed(challenge) });
     }
 
-    private static Embed MakeLaborMarketEmbed(LaborMarket laborMarket, LaborMarketRequest request, TokenContract paymentToken)
+    private static Embed MakeLaborMarketRequestEmbed(LaborMarket laborMarket, LaborMarketRequest request, TokenContract paymentToken)
     {
         return new EmbedBuilder()
             .WithColor(Color.Gold)
             .WithTitle(request.Title)
             .AddField(
                 "Reward Pool",
-                $"{MathUtils.RoundToSignificantDigits(paymentToken.DecimalsAdjust(request.PaymentTokenAmount), 4)} {paymentToken.Symbol}")
-            .AddField("Deadline to Claim", $"<t:{request.ClaimSubmitExpiration}:R>")
+                $"{MathUtils.RoundToSignificantDigits(paymentToken.DecimalsAdjust(request.PaymentTokenAmount), 4)} {paymentToken.Symbol}", true)
             .AddField("Marketplace", laborMarket.Name, true)
-            .AddField("Claim Now", $"https://metricsdao.xyz/app/market/{laborMarket.Address}/request/{request.RequestId}", true)
+            .AddField("", "")
+            .AddField("Claim to submit deadline", $"<t:{request.ClaimSubmitExpiration}:R>", true)
+            .AddField("Submission deadline", $"<t:{request.SubmitExpiration}:R>", true)
+            .AddField("Reviewer deadline", $"<t:{request.ReviewExpiration}:R>", true)
+            .AddField("Claim Now", $"https://metricsdao.xyz/app/market/{laborMarket.Address}/request/{request.RequestId}")
             .Build();
     }
 
     public async Task ShareAsync(LaborMarket laborMarket, LaborMarketRequest request, TokenContract paymentToken)
     {
-        await WebhookClient.SendMessageAsync(embeds: new[] { MakeLaborMarketEmbed(laborMarket, request, paymentToken) });
+        await WebhookClient.SendMessageAsync(
+            embeds: new[] { MakeLaborMarketRequestEmbed(laborMarket, request, paymentToken) });
     }
-
-
 }
