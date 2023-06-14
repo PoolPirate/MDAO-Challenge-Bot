91fa4ceeb6904099d6d8b53e76f48ee9cb01b73d
Playwo <eliaswolf2001@t-online.de>
2023-06-14 17:58:31 +0200

    Fix build

--- MDAO-Challenge-Bot/Entities/LaborMarketRequest.cs
+++ MDAO-Challenge-Bot/Entities/LaborMarketRequest.cs
@@ -26,8 +26,8 @@ public class LaborMarketRequest
 
     public long? TweetId { get; private set; }
 
-    public virtual LaborMarket? LaborMarket { get; private set; } //Navigation Property
-    public virtual TokenContract? PaymentToken { get; private set; } //Navigation Property
+    public virtual LaborMarket? LaborMarket { get; init; } //Navigation Property
+    public virtual TokenContract? PaymentToken { get; init; } //Navigation Property
 
     public void SetTweetId(long tweetId)
     {
